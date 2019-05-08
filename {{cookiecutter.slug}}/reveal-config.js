0e5aaa8d0e2c0ebfc97c43cdc7897b5ecfe24fb1
Florian Haas <xahteiwi@gmail.com>
2019-05-08 17:21:04 +0200

    Drop mouse event handler
    Really not useful while debugging, and seldomly handy when presenting.

--- {{cookiecutter.slug}}/reveal-config.js
+++ {{cookiecutter.slug}}/reveal-config.js
@@ -68,13 +68,3 @@ Reveal.initialize({
 {% endif %}
     ]
 });
-
-{% if cookiecutter.mouse | default(true) %}
-      window.addEventListener("mousedown", handleClick, false);
-      window.addEventListener("contextmenu", function(e) { e.preventDefault(); }, false);
-      function handleClick(e) {
-          e.preventDefault();
-          if(e.button === 0) Reveal.next();
-          if(e.button === 2) Reveal.prev();
-      }
-{% endif %}
