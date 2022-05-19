90576f57e9c4e54d0ff240379be9a277d73dfcd3
Florian Haas <xahteiwi@gmail.com>
2022-05-19 20:12:16 +0200

    Remove outdated dependencies for Reveal 4

--- {{cookiecutter.slug}}/reveal-config.js
+++ {{cookiecutter.slug}}/reveal-config.js
@@ -73,10 +73,10 @@ Reveal.initialize({
     },
 
 {% endif %}
+{% if cookiecutter.reveal_js_version | first | int < 4 %}
     // Optional libraries used to extend on reveal.js
     dependencies: [
         { src: 'reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-{% if cookiecutter.reveal_js_version | first | int < 4 %}
         { src: 'reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
         { src: 'reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
         { src: 'reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
@@ -86,7 +86,6 @@ Reveal.initialize({
         { src: '//cdn.socket.io/socket.io-1.3.5.js', async: true },
         { src: 'reveal.js/plugin/multiplex/client.js', async: true },
 {% endif %}
-{% endif %}
 {% if cookiecutter.reveal_js_menu_version != 'disable' %}
         { src: 'reveal.js-menu/menu.js', async: true, condition: function() { return !!document.body.classList; } },
 {% endif %}
@@ -95,4 +94,5 @@ Reveal.initialize({
         { src: 'reveal.js-plugins/chart/csv2chart.js' },
 {% endif %}
     ]
+{% endif %}
 });
